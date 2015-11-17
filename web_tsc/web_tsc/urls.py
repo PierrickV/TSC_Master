5d9288116ec1ef73f80909e8146c3ffbdd079a5c
Pierrick <veran@intechinfo.fr>
2015-11-17 10:45:03 +0100

    Déplacement du fichier web_tsc dans WWW

--- web_tsc/web_tsc/urls.py
+++
@@ -1,9 +0,0 @@
-#web_tsc URL Configuration
-
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-	 url(r'', include('lobby.urls')),
-	 url(r'^/home', include('lobby.urls'))
-]

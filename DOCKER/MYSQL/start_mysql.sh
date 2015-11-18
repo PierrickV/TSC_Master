66f2251692611eb00ed7309b911b21c3dee55a84
Pierrick <veran@intechinfo.fr>
2015-11-18 19:42:44 +0100

    Dockerfile web fonctionelle, reste à faire des corrections mineurs

--- DOCKER/MYSQL/start_mysql.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Pierrick VERAN - Tardigrade Security Challenge - 09/11/2015
-# Launch mysql container link with tsc_database file
-
-docker run --name tsc_database -e MYSQL_ROOT_PASSWORD="b3f672a1d0ef2d2f74366fad78faa790" -v /home/docker/mysql:/var/lib/mysql -d mysql

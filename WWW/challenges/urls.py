ca7321bf1d1db9b4d983c54d5136f138e48b26d2
Alan <alan.barthelemy@gmail.com>
2015-11-24 09:06:47 +0100

    add app challenges

--- WWW/challenges/urls.py
+++ WWW/challenges/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('challenges.views',
 	url(r'', 'home'),
-    url(r'^challenges/', 'home'),
+    url(r'^challenges/$', 'home'),
     url(r'^challenges/web/$', 'web')
 )
--- WWW/lobby/urls.py
+++ WWW/lobby/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lobby.views',
 	url(r'', 'home'),
-    url(r'home/$', 'home')
+    url(r'^home/$', 'home')
 )

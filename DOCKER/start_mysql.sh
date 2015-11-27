a228b4b7d0b850df92b0cfe364121bd671b9fad4
Alan <alan.barthelemy@gmail.com>
2015-11-27 11:03:58 +0100

    forum

--- DOCKER/start_mysql.sh
+++ DOCKER/start_mysql.sh
@@ -5,6 +5,6 @@
 docker run \
  --name tsc_database \
  -e MYSQL_ROOT_PASSWORD="zIDCUiY55YwBOkQKbML2"  \
- -v /home/docker/mysql:/var/lib/mysql \
+ -v //c/wamp/www/Intech/Projets/S5/PI/Web/tardigrade_security_challenge/WWW:/var/lib/mysql \
  -p 127.0.0.1:3306:3306 \
  -d mysql
--- DOCKER/start_nginx.sh
+++ DOCKER/start_nginx.sh
@@ -2,11 +2,9 @@
 # Pierrick VERAN - Tardigrade Security Challenge - 09/11/2015
 # Launch nginx-django container link with tsc's webfiles
 
-django_www_files="/home/lilyus/Git/TSC/WWW:/var/www:ro"
-
 docker run -it \
  --name tsc_web \
- -v $django_www_files \
+ -v //c/wamp/www/Intech/Projets/S5/PI/Web/tardigrade_security_challenge/WWW:/var/www:ro \
  -p 127.0.0.1:80:80 \
  -d tsc/master:Web \
  /bin/bash

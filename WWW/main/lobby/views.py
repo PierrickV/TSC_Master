8e6e5d93366f034eeaf1c9eae9d5ef735b964b40
Pierrick <veran@intechinfo.fr>
2015-12-18 09:47:24 +0100

    Isolation du site principale et du forum: Le site principale foncitonne

---
+++ WWW/main/lobby/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'lobby/base.html', locals())
+
+
+def form(request):
+	if request.method == 'GET':
+		form = ConnectForm()
+	else:
+        # A Connect request: Handle Form Upload
+		form = ConnectForm(request.POST) # Bind data from request.POST into a PostForm
+ 
+        # If data is valid, proceeds to create a new post and redirect the user
+		#if form.is_valid():
+ 
+	return render(request, 'lobby/base.html', {
+		'form': form,
+    })

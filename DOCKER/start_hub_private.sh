d190d978dc8d9efd2c3b73a0e905c1b8376e5562
Pierrick <veran@intechinfo.fr>
2015-12-22 15:16:12 +0100

    Ajout du hub privé

--- DOCKER/start_hub_private.sh
+++ DOCKER/start_hub_private.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+# Pierrick VERAN - Tardigrade Security Challenge - 22/12/2015
+# Launch hub container for private docker images storage
+# docker pull distribution/registry:master
 
-docker rm -f tsc_hub_public
+docker rm -f tsc_hub_private
 
 docker run \
         --name tsc_hub_private \
         --restart=always \
         -p 127.0.0.1:5000:5000 \
-        -v /home/lilyus/Hub:/var/lib/registry \
-        -v `pwd`/certs:/certs \
-        -e REGISTRY_HTTP_TLS_CERTIFICATE=/HUB/certs/domain.crt \
-        -e REGISTRY_HTTP_TLS_KEY=/HUB/certs/domain.key \
+        -v $(pwd)/HUB/auth:/auth \
+        -e REGISTRY_AUTH="htpasswd" \
+        -e REGISTRY_AUTH_HTPASSWD_REALM="Registry Realm" \
+        -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+        -v /home/lilyus/hub:/var/lib/registry:rw \
+        -v $(pwd)/HUB/certs:/certs:ro \
+        -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+        -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key \
+        -e REGISTRY_HTTP_SECRET="2J9r9mtbm4BdRmcV4ExgVzEbKzkW4W7V8EUYm9cMQ8UGu" \
         -d registry:2
 

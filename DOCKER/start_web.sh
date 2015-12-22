e5bb4d4efc13621e7e475db86baa477494b65802
Pierrick <veran@intechinfo.fr>
2015-12-22 18:04:46 +0100

    Ajout du sshfs et d'une nouvelle paire de clés RSA pour tsc_web

--- DOCKER/start_web.sh
+++ DOCKER/start_web.sh
@@ -5,12 +5,13 @@
 docker rm -f tsc_web
 
 docker run -it \
---name tsc_web \
---restart=always \
--v /home/docker/Git/TSC/WWW:/var/www:rw \
--p 80:80 \
---link tsc_database \
--d tsc/master:Web
+        --name tsc_web \
+        --restart=always \
+        -v /home/docker/Git/TSC/WWW:/var/www:rw \
+        --cap-add SYS_ADMIN --device /dev/fuse \
+        -p 80:80 \
+        --link tsc_database \
+        -d tsc/master:Web
 
 
 

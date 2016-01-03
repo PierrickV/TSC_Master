ddbf7fa856d58e4e19ca2471d23f6beccff70619
Pierrick <veran@intechinfo.fr>
2016-01-03 22:33:09 +0100

    Ajout de l'https

--- DOCKER/start_web.sh
+++ DOCKER/start_web.sh
@@ -10,6 +10,7 @@ docker run -it \
 -v /home/alababa/git/tardigrade_security_challenge/WWW/main:/var/www/main:rw \
 -v /home/alababa/nv_challenges:/nv_challenges:rw \
 --privileged --cap-add=MKNOD --cap-add=SYS_ADMIN --device=/dev/fuse \
+-p 127.0.0.1:443:443 \
 -p 127.0.0.1:80:80 \
 --link tsc_database \
 -d tsc/master:Web

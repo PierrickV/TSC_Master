5aa8e112fe8a221e9d999d41bab639d07456b198
Pierrick <veran@intechinfo.fr>
2016-01-05 09:49:02 +0100

    Ajout des certificats pour les registry public et privés

--- DOCKER/start_web.sh
+++ DOCKER/start_web.sh
@@ -7,8 +7,8 @@ docker rm -f tsc_web
 docker run -it \
     --name tsc_web \
     --restart=always \
-    -v /home/docker/Git/TSC/WWW:/var/www:rw \
-    -v /home/docker/nv_challenges:/nv_challenges:rw \
+    -v /home/alababa/git/tardigrade_security_challenge/WWW/main:/var/www/main:rw \
+    -v /home/alababa/nv_challenges:/nv_challenges:rw \
     --privileged --cap-add=MKNOD --cap-add=SYS_ADMIN --device=/dev/fuse \
     -p 127.0.0.1:443:443 \
     -p 127.0.0.1:80:80 \

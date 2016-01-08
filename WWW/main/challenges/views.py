1fa07f215eb22e622ebc8d5781c28b5e0175fb4d
Alababa <alan.barthelemy@gmail.com>
2016-01-08 09:49:07 +0100

    modif maquette

--- WWW/main/challenges/views.py
+++ WWW/main/challenges/views.py
@@ -31,8 +31,9 @@ def add(request):
 		elif typeadd == 'zip':
 			typeadd = '2'
 			fichier = request.FILES['challenge']
+			name = request.FILES['challenge'].name
 			#shutil.move(fichier, '/nv_challenge')
-			new_challenge = Challenges(name = name, creator = username, description = description, category = category, level = difficulty, type_upload = typeadd, status = 'prive', process = 'non_valide', clue = indice, url = 'aucun', points = '10')
+			new_challenge = Challenges(name = name, creator = username, description = description, category = category, level = difficulty, type_upload = typeadd, status = 'prive', process = 'non_valide', clue = indice, url = name, points = '10')
 			new_challenge.save()
 		else:
 			typeadd = '3'

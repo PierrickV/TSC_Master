b734aa25edd22f4038c79604f9dbfdaac95de5ee
Pierrick <veran@intechinfo.fr>
2016-01-22 10:16:24 +0100

    Début des actions admin: Rendre public

--- WWW/main/lobby/admin.py
+++ WWW/main/lobby/admin.py
@@ -2,37 +2,50 @@ from django.contrib import admin
 from lobby.models import *
 from django.db import models
 from django.contrib.auth.models import UserManager
-from os import rename
+import os
 
 import pprint
+
+
+public_file_path="validated/public/file/"
+public_docker_path="validated/public/docker/"
+private_file_path="validated/private/file/"
+private_file_path= 'validated/private/docker/'
 
 # Declaration des classes precisant l'affichage
 class ProfilAdmin(admin.ModelAdmin):
    list_display   = ('username', 'status', 'role', 'description')
 
 class ChallengeAdmin(admin.ModelAdmin):
-    list_display   = ('name', 'creator', 'description', 'file', 'category', 'level', 'status','process','clue','token')
-    actions = ['make_public']
+    list_display   = ('name', 'creator', 'description', 'file', 'url', 'category', 'level', 'status','process','clue','token')
+    actions = ['make_public','make_valide']
 
-    def make_public(self, request, queryset):
-
-        #Changement de la du status en public
+    #Changement du status en "public"
+    def make_public(self, request, queryset):    #Rendre public
         queryset.update(status='public')
-        '''
         #Deplacement du fichier
         for key in queryset.values_list('file'):
-            filename=key
-            self.message_user(request, "validated/public/docker/%s" % filename)
-            queryset.update(status="validated/public/docker/%s" % filename)
-            #os.rename("%s" %filename ,"/media/validated/public/docker/%s" % filename)
+            src_path= ''.join(key) #tuple to string
+            filename= os.path.basename(src_path)
+            dst_path=public_file_path + "%s" % filename
 
-        #Deplacement du docker
-        for key in queryset.values_list('file'):
-            self.message_user(request, key)
-            #os.rename("","/media/validated/")
-        '''
+            self.message_user(request,  "moving %s from %s to %s" % (filename,src_path,dst_path,))
+            #os.rename(src_path ,dst_path)
+            #queryset.update(file=dst_path)
 
     make_public.short_description = "Rendre publique"
+
+    #Rendre public
+    #Rendre prive
+    #Valider et rendre public
+    #Valider et rendre prive
+
+    #Changement du process en "valide"
+    def make_valide(self, request, queryset):
+
+        #Changement du status en public
+        queryset.update(process='valide')
+    make_valide.short_description = "Valider"
 
 class EventAdmin(admin.ModelAdmin):
    list_display   = ('name', 'description', 'place', 'training_event', 'date_start','date_end')
